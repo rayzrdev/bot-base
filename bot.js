0880bea85032d5c037caae5da250a7ad3172f145
Rayzr522 <rayzr522@gmail.com>
2020-07-18 11:50:55 -0400

    Set up and use eslint

--- bot.js
+++ bot.js
@@ -1,69 +1,69 @@
 // Built-in libraries from Node.JS
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 // Only import the Client class from Discord.js
-const { Client } = require('discord.js');
+const { Client } = require('discord.js')
 
-require('dotenv').config();
+require('dotenv').config()
 
 // Super fancy config loader/validator
 const config = (() => {
-    const token = process.env.BOT_TOKEN;
+    const token = process.env.BOT_TOKEN
 
     // If there isn't a token, the bot won't start, but if there is then
     // we want to make sure it's a valid bot token
     if (!token) {
-        console.error('Missing BOT_TOKEN environment variable');
-        process.exit(1);
+        console.error('Missing BOT_TOKEN environment variable')
+        process.exit(1)
     }
 
-    if (!/^[a-zA-Z0-9_\.\-]{59}$/.test(token)) {
+    if (!/^[a-zA-Z0-9_.-]{59}$/.test(token)) {
         console.error('Invalid bot token!')
-        process.exit(1);
+        process.exit(1)
     }
 
-    const prefix = process.env.BOT_PREFIX;
+    const prefix = process.env.BOT_PREFIX
 
     if (!prefix) {
-        console.error('Missing BOT_PREFIX environment variable');
-        process.exit(1);
+        console.error('Missing BOT_PREFIX environment variable')
+        process.exit(1)
     }
 
-    return { token, prefix };
-})();
+    return { token, prefix }
+})()
 
 // Store the commands in a Map (slightly better than a raw object)
-const commands = new Map();
+const commands = new Map()
 // Create the client
-const bot = new Client({ disableEveryone: true });
+const bot = new Client({ disableEveryone: true })
 
 // Store the config and commands on the bot variable so as to make them
 // easily accessible in commands and other files
-bot.config = config;
-bot.commands = commands;
+bot.config = config
+bot.commands = commands
 
 // Read every file in ./commands and filter out the non-JS files
 fs.readdirSync(path.resolve(__dirname, 'commands'))
     .filter(f => f.endsWith('.js'))
     .forEach(f => {
         // Attempt to load the file
-        console.log(`Loading command ${f}`);
+        console.log(`Loading command ${f}`)
         try {
             // Require the raw file
-            let command = require(`./commands/${f}`);
+            let command = require(`./commands/${f}`)
             // Validate that there's a run function and a valid help object
             if (typeof command.run !== 'function') {
-                throw 'Command is missing a run function!';
+                throw 'Command is missing a run function!'
             } else if (!command.help || !command.help.name) {
-                throw 'Command is missing a valid help object!';
+                throw 'Command is missing a valid help object!'
             }
             // Store the command in the map based on its name
-            commands.set(command.help.name, command);
+            commands.set(command.help.name, command)
         } catch (error) {
             // Log any errors from the validator or from requiring the file
-            console.error(`Failed to load command ${f}: ${error}`);
+            console.error(`Failed to load command ${f}: ${error}`)
         }
-    });
+    })
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} (ID: ${bot.user.id})`)
@@ -90,38 +90,38 @@ bot.on('ready', () => {
         // 'DEAFEN_MEMBERS',
     ]).then(invite => {
         // After generating the invite, log it to the console
-        console.log(`Click here to invite the bot to your guild:\n${invite}`);
-    });
-});
+        console.log(`Click here to invite the bot to your guild:\n${invite}`)
+    })
+})
 
 bot.on('message', message => {
     // Ignore messages from bots and from DMs (non-guild channels)
     if (message.author.bot || !message.guild) {
-        return;
+        return
     }
 
     // Just a shorthand variable
-    let { content } = message;
+    let { content } = message
     // Ignore any messages that don't start with the configurable prefix
     if (!content.startsWith(config.prefix)) {
-        return;
+        return
     }
 
     // Take all the text after the prefix and split it into an array,
     // splitting at every space (so 'hello world' becomes ['hello', 'world'])
-    let split = content.substr(config.prefix.length).split(' ');
+    let split = content.substr(config.prefix.length).split(' ')
     // Get the command label (which is the first word after the prefix)
-    let label = split[0];
+    let label = split[0]
     // Get the rest of the words after the prefix
-    let args = split.slice(1);
+    let args = split.slice(1)
 
     // If there's a command with that given label...
     if (commands.get(label)) {
         // ... get the command with that label and run it with the bot, the
         // message variable, and the args as parameters
-        commands.get(label).run(bot, message, args);
+        commands.get(label).run(bot, message, args)
     }
-});
+})
 
 // Only run the bot if the token was provided
-config.token && bot.login(config.token);
+config.token && bot.login(config.token)
--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 exports.run = (bot, msg, args) => {
     msg.channel.send(':watch: | Ping!').then(m => {
-        m.edit(`:watch: | Pong! \`${m.createdTimestamp - msg.createdTimestamp}ms\``);
-    });
-};
+        m.edit(`:watch: | Pong! \`${m.createdTimestamp - msg.createdTimestamp}ms\``)
+    })
+}
 
 exports.help = {
     name: 'ping',
     usage: 'ping',
     description: 'Pings the bot to check its connection speed.'
-};
+}
